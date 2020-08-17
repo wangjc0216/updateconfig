545dbd0fc48332825af6311046f73903b6bd6e8a
jcwang <wangjc0216@outlook.com>
2020-08-17 13:42:32 +0800

    [更新通道demo]test用例增加注释

--- main_test.go
+++ main_test.go
@@ -17,26 +17,32 @@ var (
 
 //应用通道增加组织
 func TestADDORG(t *testing.T) {
+	//实例化"第三家"组织，传入交易的路径
 	crossG, err := org.NewCfgGroup(crossName, org.WithTxPath(txpath))
 	if err != nil {
 		t.Fatal(err)
 	}
+	//实例化Org1
 	org1G, err := org.NewCfgGroup(org1Name, org.WithSDKPath(org1CfgPath))
 	if err != nil {
 		t.Fatal(err)
 	}
+	//获取mychannel中最新配置
 	latestedCfg1, err := org1G.GetChannelConfig(MyChannel)
 	if err != nil {
 		t.Fatal(err)
 	}
+	//根据配置文件，生成第三家组织的配置
 	crossCfgGroup, err := crossG.GenCfgGroupFromTx()
 	if err != nil {
 		t.Fatal(err)
 	}
+	// 获取mychannel中最新配置，这里重复获取两次是为了接下来的比对
 	latestedCfg2, err := org1G.GetChannelConfig(MyChannel)
 	if err != nil {
 		t.Fatal(err)
 	}
+	//组装最新的通道配置
 	latestedCfg2.ChannelGroup.Groups["Application"].Groups["CrosshubMSP"] = crossCfgGroup
 
 	cfgUpdate, err := org.GetCompute(latestedCfg1, latestedCfg2, MyChannel)
@@ -47,6 +53,7 @@ func TestADDORG(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	//Org1,Org2 为最新通道配置签名
 	sig1, err := org1G.CreateConfigSignature(envBytes)
 	if err != nil {
 		t.Fatal(err)
@@ -59,6 +66,7 @@ func TestADDORG(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	//两个签名传给Org1，由Org1来发起更新通道配置的交易
 	err = org1G.SaveChannel(MyChannel, envBytes, sig1, sig2)
 	if err != nil {
 		t.Fatal(err)
